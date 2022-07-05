953f1250579ba5c5da47c87f034609173681aa39
Amira de Moya <demoyaamira@gmail.com>
2022-07-04 20:36:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-function divide(x,y){
+/*function divide(x,y){
    return x/y
 }
 divide(2000, 100)
@@ -11,4 +11,13 @@ const square = x => x ** x;
 square(2)
 
 
-const add = (p1, p2) => p1 + p2
+const add = (p1, p2) => p1 + p2*?*/
+
+
+// Redo Arrow Functions
+
+const divide = (x, y) => x/y;
+
+const square = x => x * x;
+
+const add = (x, y) => x + y;
